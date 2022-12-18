608851b757bca4b90c395a79f59550e391700bf3
capcom6 <capcom6@users.noreply.github.com>
2022-12-18 10:07:53 +0000

    deploy: 2557a4c08402333c7a4bc10f8303c36b32938097

--- docs.go
+++ docs.go
@@ -506,7 +506,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v0.3.0",
+	Version:          "v0.4.0",
 	Host:             "localhost:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
